9454add0dcee516dea5f07d7501b7b7afd5971ed
dineshsaratha8 <dineshsaratha8@gmail.com>
2019-11-16 22:44:44 +0530

    Basic operation implemented

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -18,7 +18,96 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Widget createButton(buttonText,int flex) {
+  double num1, num2, calculatedOutput;
+  String operator, output;
+
+  _MyHomePageState() {
+    calculatedOutput = 0;
+    output = "0";
+  }
+
+  void onButtonPressed(String buttonText) {
+    print(buttonText);
+    switch (buttonText) {
+      case "C":
+        //Clear the data
+        num1 = 0;
+        num2 = 0;
+        calculatedOutput = 0;
+        output = "0";
+        break;
+      case "+":
+      case "-":
+      case "x":
+      case "÷":
+      case "%":
+        //Store current output as num1 and update operator
+        num1 = double.parse(output);
+        operator = buttonText;
+        output = "0";
+        break;
+      case ".":
+        //Insert . if not already exist
+        if (output.contains(".")) {
+          //Toast error message
+        } else {
+          output += ".";
+        }
+        break;
+      case "N":
+        //Toast functionality not available
+        break;
+      case "=":
+        // Do calculation
+        num2 = double.parse(output);
+        calculatedOutput = getCalculatedOutput();
+        print(calculatedOutput);
+        output = calculatedOutput.toString();
+        break;
+      default:
+        print("Blah $output");
+        //Append the number
+        if (output == "0") {
+          //If its already 0 don't append the number
+          output = buttonText;
+          break;
+        }
+        //Clean output if it contain text,reset the text
+        RegExp regExp=RegExp('^[a-zA-Z]*');
+        if (regExp.hasMatch(output)) {
+          print("Matched");
+          output = "";
+        }
+        output += buttonText;
+    }
+    setState(() {
+      //To refresh the widget
+    });
+  }
+
+  double getCalculatedOutput() {
+    switch (operator) {
+      case "+":
+        //add the number
+        return num1 + num2;
+      case "-":
+        //Subtract the number
+        return num1 - num2;
+      case "÷":
+        //Divide the number
+        return num1 / num2;
+      case "x":
+        //Multiply the number
+        return num1 * num2;
+      case "%":
+        //Return remainder
+        return num1 % num2;
+      default:
+      //Toast Wrong operator error
+    }
+  }
+
+  Widget createButton(buttonText, int flex) {
     return Expanded(
       flex: flex,
       child: FlatButton(
@@ -27,7 +116,7 @@ class _MyHomePageState extends State<MyHomePage> {
           buttonText,
           style: TextStyle(fontSize: 25),
         ),
-        onPressed: () {},
+        onPressed: () => onButtonPressed(buttonText),
         color: Colors.blueGrey,
         textColor: Colors.white,
       ),
@@ -45,9 +134,9 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Container(
               width: double.infinity,
-              margin: EdgeInsets.fromLTRB(0,20,0,0),
+              margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
               child: Text(
-                '0',
+                output,
                 textAlign: TextAlign.right,
                 style: TextStyle(
                     color: Colors.white,
@@ -63,41 +152,41 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
                 Row(
                   children: <Widget>[
-                    createButton('C',1),
-                    createButton('N',1),
-                    createButton('%',1),
-                    createButton('/',1)
+                    createButton('C', 1),
+                    createButton('N', 1),
+                    createButton('%', 1),
+                    createButton('÷', 1)
                   ],
                 ),
                 Row(
                   children: <Widget>[
-                    createButton('7',1),
-                    createButton('8',1),
-                    createButton('9',1),
-                    createButton('X',1)
+                    createButton('7', 1),
+                    createButton('8', 1),
+                    createButton('9', 1),
+                    createButton('x', 1)
                   ],
                 ),
                 Row(
                   children: <Widget>[
-                    createButton('4',1),
-                    createButton('5',1),
-                    createButton('6',1),
-                    createButton('-',1)
+                    createButton('4', 1),
+                    createButton('5', 1),
+                    createButton('6', 1),
+                    createButton('-', 1)
                   ],
                 ),
                 Row(
                   children: <Widget>[
-                    createButton('1',1),
-                    createButton('2',1),
-                    createButton('3',1),
-                    createButton('+',1)
+                    createButton('1', 1),
+                    createButton('2', 1),
+                    createButton('3', 1),
+                    createButton('+', 1)
                   ],
                 ),
                 Row(
                   children: <Widget>[
-                    createButton('Clear',2),
-                    createButton('.',1),
-                    createButton('=',1),
+                    createButton('0', 2),
+                    createButton('.', 1),
+                    createButton('=', 1),
                   ],
                 ),
               ],
@@ -108,3 +197,9 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+//TODO
+//1. decimal bug is there
+//2. One operation missing
+//3. UI perfection
+//4. Add additional feature
