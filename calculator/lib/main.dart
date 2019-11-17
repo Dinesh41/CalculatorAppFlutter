a6cca76e23824bb0bc9b2ea1e57df215631cf80a
dineshsaratha8 <dineshsaratha8@gmail.com>
2019-11-17 11:21:16 +0530

    Phone and Tablet Support added

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp]).then((_) {
+    runApp(MyApp());
+  });
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -29,11 +36,9 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void onButtonPressed(String buttonText) {
-    print(buttonText);
     //Clean output if it contain text,reset the text
     RegExp regExp = RegExp('^[a-zA-Z]*\$');
     if (regExp.hasMatch(output)) {
-      print("Matched");
       output = "0";
     }
     switch (buttonText) {
@@ -41,6 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
         //Clear the data
         num1 = 0;
         num2 = 0;
+        operator=null;
         calculatedOutput = 0;
         output = "0";
         break;
@@ -55,30 +61,30 @@ class _MyHomePageState extends State<MyHomePage> {
         output = "0";
         break;
       case '±':
-        //change number to opposite sign
+        //If output is 0 , dont do anything
         double currentOutput = double.parse(output);
+        if(currentOutput==0){
+          break;
+        }
+        //change number to opposite sign
         output = (currentOutput > 0) ? "-$output" : output.substring(1);
         break;
       case ".":
         //Insert . if not already exist
         if (output.contains(".")) {
           //Toast error message
+          Fluttertoast.showToast(msg: "Already decimal number exits, to remove decimal, click C button");
         } else {
           output += ".";
         }
-        break;
-      case "±":
-        //Toast functionality not available
         break;
       case "=":
         // Do calculation
         num2 = double.parse(output);
         calculatedOutput = getCalculatedOutput();
-        print(calculatedOutput);
         output = calculatedOutput.toString();
         break;
       default:
-        print("Blah $output");
         //Append the number
         if (output == "0") {
           //If its already 0 don't append the number
@@ -113,6 +119,7 @@ class _MyHomePageState extends State<MyHomePage> {
         return num1 % num2;
       default:
       //Toast Wrong operator error
+        Fluttertoast.showToast(msg: "Wrong Operator Found");
     }
   }
 
@@ -120,6 +127,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Expanded(
       flex: flex,
       child: MaterialButton(
+        height: double.infinity,
         shape: RoundedRectangleBorder(
             side: BorderSide(width: 2, color: Color.fromRGBO(39, 57, 79, 1)),
             borderRadius: BorderRadius.circular(10)),
@@ -146,6 +154,7 @@ class _MyHomePageState extends State<MyHomePage> {
       backgroundColor: Color.fromRGBO(39, 57, 79, 1),
       body: Container(
         width: double.infinity,
+        height: double.infinity,
         child: Column(
           children: <Widget>[
             Expanded(
@@ -172,44 +181,55 @@ class _MyHomePageState extends State<MyHomePage> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      createButton('C', 1, Color.fromRGBO(85, 90, 94, 1)),
-                      createButton('±', 1, Color.fromRGBO(85, 90, 94, 1)),
-                      createButton('%', 1, Color.fromRGBO(85, 90, 94, 1)),
-                      createButton('÷', 1, Color.fromRGBO(28, 85, 115, 1))
-                    ],
+                  Spacer(),
+                  Expanded(
+                    child: Row(
+                      children: <Widget>[
+                        createButton('C', 1, Color.fromRGBO(85, 90, 94, 1)),
+                        createButton('±', 1, Color.fromRGBO(85, 90, 94, 1)),
+                        createButton('%', 1, Color.fromRGBO(85, 90, 94, 1)),
+                        createButton('÷', 1, Color.fromRGBO(28, 85, 115, 1))
+                      ],
+                    ),
                   ),
-                  Row(
-                    children: <Widget>[
-                      createButton('7', 1, Color.fromRGBO(124, 122, 125, 1)),
-                      createButton('8', 1, Color.fromRGBO(124, 122, 125, 1)),
-                      createButton('9', 1, Color.fromRGBO(124, 122, 125, 1)),
-                      createButton('x', 1, Color.fromRGBO(28, 85, 115, 1))
-                    ],
+                  Expanded(
+                    child: Row(
+                      children: <Widget>[
+                        createButton('7', 1, Color.fromRGBO(124, 122, 125, 1)),
+                        createButton('8', 1, Color.fromRGBO(124, 122, 125, 1)),
+                        createButton('9', 1, Color.fromRGBO(124, 122, 125, 1)),
+                        createButton('x', 1, Color.fromRGBO(28, 85, 115, 1))
+                      ],
+                    ),
                   ),
-                  Row(
-                    children: <Widget>[
-                      createButton('4', 1, Color.fromRGBO(124, 122, 125, 1)),
-                      createButton('5', 1, Color.fromRGBO(124, 122, 125, 1)),
-                      createButton('6', 1, Color.fromRGBO(124, 122, 125, 1)),
-                      createButton('-', 1, Color.fromRGBO(28, 85, 115, 1))
-                    ],
+                  Expanded(
+                    child: Row(
+                      children: <Widget>[
+                        createButton('4', 1, Color.fromRGBO(124, 122, 125, 1)),
+                        createButton('5', 1, Color.fromRGBO(124, 122, 125, 1)),
+                        createButton('6', 1, Color.fromRGBO(124, 122, 125, 1)),
+                        createButton('-', 1, Color.fromRGBO(28, 85, 115, 1))
+                      ],
+                    ),
                   ),
-                  Row(
-                    children: <Widget>[
-                      createButton('1', 1, Color.fromRGBO(124, 122, 125, 1)),
-                      createButton('2', 1, Color.fromRGBO(124, 122, 125, 1)),
-                      createButton('3', 1, Color.fromRGBO(124, 122, 125, 1)),
-                      createButton('+', 1, Color.fromRGBO(28, 85, 115, 1))
-                    ],
+                  Expanded(
+                    child: Row(
+                      children: <Widget>[
+                        createButton('1', 1, Color.fromRGBO(124, 122, 125, 1)),
+                        createButton('2', 1, Color.fromRGBO(124, 122, 125, 1)),
+                        createButton('3', 1, Color.fromRGBO(124, 122, 125, 1)),
+                        createButton('+', 1, Color.fromRGBO(28, 85, 115, 1))
+                      ],
+                    ),
                   ),
-                  Row(
-                    children: <Widget>[
-                      createButton('0', 2, Color.fromRGBO(124, 122, 125, 1)),
-                      createButton('.', 1, Color.fromRGBO(85, 90, 94, 1)),
-                      createButton('=', 1, Color.fromRGBO(28, 85, 115, 1)),
-                    ],
+                  Expanded(
+                    child: Row(
+                      children: <Widget>[
+                        createButton('0', 2, Color.fromRGBO(124, 122, 125, 1)),
+                        createButton('.', 1, Color.fromRGBO(85, 90, 94, 1)),
+                        createButton('=', 1, Color.fromRGBO(28, 85, 115, 1)),
+                      ],
+                    ),
                   ),
                 ],
               ),
@@ -220,8 +240,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-//TODO
-//2. One operation missing
-//3. UI perfection
-//4. Add additional feature
